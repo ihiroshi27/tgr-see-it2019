1d57da8998dc3fb5633839bf18156c86d21ca71f
Perayuth N. Thanyanon <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-08 13:58:01 +0700

    Initial Project

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/listUsers', function(req, res) {
+    fs.readFile(path.join(__dirname, "users.json"), 'utf8', function(err, data) {
+        let json = JSON.parse(data);
+        res.json(json);
+    });
+});
+
+var server = app.listen(8080, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("App listening at http://%s:%s", host, port)
+ })
