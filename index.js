fcd366a6e9c8fc3b7228c485c9d2b404bf2d623c
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-09 13:28:57 +0700

    Add mongodb connection

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var fs = require("fs");
 var bodyParser = require("body-parser");
 var path = require("path");
 var http = require("http");
+var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://tgr:tgr2019@localhost:27017/tgr2019', { useNewUrlParser: true });
 
 var app = express();
 app.use(bodyParser.json());
