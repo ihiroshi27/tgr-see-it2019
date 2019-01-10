3c677a8f9e662f7433ed847399d980ef40ac5789
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-10 22:48:05 +0700

    Add custom last hour for admin-mon

--- controller/lora.js
+++ controller/lora.js
@@ -26,6 +26,29 @@ router.get('/admin-mon', function(req, res, next) {
     });
 });
 
+router.get('/admin-mon/:hours', function(req, res, next) {
+    var hours = req.params.hours;
+    var lastHour = new Date();
+    lastHour.setHours(lastHour.getHours() - hours);
+    Lora.find({ datetime: { $gt: lastHour } }, function(err, records) {
+        if (err) next(err);
+        else {
+            let peopleIn = 0, peopleOut = 0;
+            records.forEach(function(record) {
+                peopleIn += record.sensor.peopleIn;
+                peopleOut += record.sensor.peopleOut;
+            });
+
+            res.json({
+                temperature: records[0].sensor.temperature,
+                humidity: records[0].sensor.humidity,
+                peopleIn: peopleIn,
+                peopleOut: peopleOut
+            });
+        }
+    });
+});
+
 router.post('/', function(req, res, next) {
     let uplink = req.body.DevEUI_uplink;
     let frames = uplink.payload_parsed.frames;
