5f1b8a2ac49c7f43f358b636d9aa969702d1c67e
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-10 16:58:46 +0700

    Add admin-mon

--- controller/lora.js
+++ controller/lora.js
@@ -4,6 +4,28 @@ var router = express.Router();
 
 var Lora = require("../model/lora");
 
+router.get('/amin-mon', function(req, res, next) {
+    var lastHour = new Date();
+    lastHour.setHours(lastHour.getHours() - 1);
+    Lora.find({ datetime: { $gt: lastHour } }, function(err, records) {
+        if (err) next(err);
+        else {
+            let peopleIn = 0, peopleOut = 0;
+            records.forEach(function(record) {
+                peopleIn += record.sensor.peopleIn;
+                peopleOut += record.sensor.peopleOut;
+            });
+
+            res.json({
+                temperature: records[0].sensor.temperature,
+                humidity: records[0].sensor.humidity,
+                peopleIn: peopleIn,
+                peopleOut: peopleOut
+            });
+        }
+    });
+});
+
 router.post('/', function(req, res, next) {
     let uplink = req.body.DevEUI_uplink;
     let frames = uplink.payload_parsed.frames;
