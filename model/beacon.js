ece06d8c99fb9c5cc3518e9ebe49de61b723138e
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-11 00:03:58 +0700

    Add userID

--- model/beacon.js
+++ model/beacon.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({ 
     datetime: { type: Date, required: true },
-    status: { type: String, required: true }
+    status: { type: String, required: true },
+    userID: { type: String, required: true }
 });
 
 module.exports = mongoose.model('beacon', schema);
