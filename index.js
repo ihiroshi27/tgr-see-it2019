96d4babd6767a055c8854ca89510a8c6136b7158
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-10 11:20:15 +0700

    Initial Integration Project

--- index.js
+++ index.js
@@ -5,76 +5,10 @@ var path = require("path");
 var http = require("http");
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://tgr:tgr2019@localhost:27017/hwData', { useNewUrlParser: true });
+mongoose.connect('mongodb://tgr:tgr2019@localhost:27017/seeit', { useNewUrlParser: true });
 
 var app = express();
 app.use(bodyParser.json());
-
-var Temperature = require('./model/temperature');
-
-function validate(data) {
-    if (typeof(data.teamID) == "undefined") {
-        return new Error('Missing teamID');
-    } else if (typeof(data.temp) === "undefined") {
-        return new Error('Missing temp');
-    } else {
-        return null;
-    }
-}
-
-app.post('/receiveData', function(req, res, next) {
-    var frames = req.body.DevEUI_uplink.payload_parsed.frames;
-    let data = {};
-    frames.forEach(function(frame) {
-        if (frame.typeString == "Analog Input") {
-            data.teamID = frame.value;
-        }
-        if (frame.typeString == "Temperature Sensor") {
-            data.temp = frame.value;
-        }
-    });
-
-    var err = validate(data);
-    if (err) next(err);
-    else Temperature.findOneAndUpdate({ teamID: data.teamID }, data, { upsert: true }, function(err) {
-        if (err) next(err);
-        else res.json({ "results": "Complete" });
-    });
-});
-
-app.get('/showData', function(req, res, next) {
-    Temperature.find(null, function(err, record) {
-        if (err) next(err);
-        else res.json({ results: record });
-    });
-});
-
-app.post('/addData', function(req, res, next) {
-    var data = req.body;
-    var err = validate(data);
-    if (err) next(err);
-    else Temperature.findOneAndUpdate({ teamID: data.teamID }, data, { upsert: true }, function(err) {
-        if (err) next(err);
-        else res.json({ "results": "Complete" });
-    });
-});
-
-app.put('/editData/:teamID', function(req, res, next) {
-    var teamID = req.params.teamID;
-    var temp = req.body.temp;
-    Temperature.updateOne({ teamID: teamID }, { $set: { temp: temp } }, function(err) {
-        if (err) next(err);
-        else res.json({ "results": "Complete" });
-    });
-});
-
-app.delete('/deleteData/:teamID', function(req, res, next) {
-    var teamID = req.params.teamID;
-    Temperature.deleteOne({ teamID: teamID }, function(err) {
-        if (err) next(err);
-        else res.json({ "results": "Complete" });
-    });
-});
 
 var httpServer = http.createServer(app);
 httpServer.listen(80);
--- model/temperature.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = mongoose.Schema({ 
-    teamID: { type: Number, required: true },
-    temp: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('temperature', schema);
