92349fd111b0cf4cd94f48658beaa7c2e84aba72
ihiroshi27 <ihiroshi27@Hiroshis-MacBook-Pro-15.local>
2019-01-10 23:00:36 +0700

    Fix incorrect image src

--- view/src/NavBar.js
+++ view/src/NavBar.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 import './NavBar.css';
 
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
 class Navbar extends React.Component {
     render() {
         return (
@@ -11,9 +13,9 @@ class Navbar extends React.Component {
                     See-It2019
                 </div>
                 <div className="section">
-                    <NavLink className="link" activeClassName="active" exact to="/"><img src="/dashboard-icon.png" />Dashboard</NavLink>
-                    <NavLink className="link" activeClassName="active" exact to="/beacon"><img src="/line-beacon.png" />Line Beacon</NavLink>
-                    <NavLink className="link" activeClassName="active" exact to="/lora"><img src="/lora-icon.png" />LoRa</NavLink>
+                    <NavLink className="link" activeClassName="active" exact to="/"><img src={ PUBLIC_URL + "/dashboard-icon.png" } />Dashboard</NavLink>
+                    <NavLink className="link" activeClassName="active" exact to="/beacon"><img src={ PUBLIC_URL + "/line-beacon.png" } />Line Beacon</NavLink>
+                    <NavLink className="link" activeClassName="active" exact to="/lora"><img src={ PUBLIC_URL + "/lora-icon.png" } />LoRa</NavLink>
                 </div>
             </div>
         );
